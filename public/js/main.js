57407b4be60d5d8aeda212af77a7717a8b5e6b51
paulmis <chugdevs@gmail.com>
2021-01-18 13:39:59 +0100

    added board generation and removed the board image, cleaned up main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,47 @@
+// @ts-check
+
+// Returns the color and piece type of the initial piece at a given position
+function InitialPiece(col, row) {
+    // Get color
+    this.color = row > 4 ? 'black' : 'white';
+
+    // Get piece type
+    if (row > 4) row = 9 - row;
+    if (row == 2)
+        this.type = 'pawn';
+    else if (row == 1) {
+        const figures = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'];
+        this.type = figures[col - 1];
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    const board = document.querySelector('.board')
+    const tiles = document.querySelector('.tiles')
     const pieces = document.querySelector('.pieces')
 
-    for(x = 0; x < 8; x++) {
-        var blackPawn = document.createElement('img');
-        blackPawn.setAttribute('src', 'img/pieces/black/pawn.svg')
-        blackPawn.setAttribute('class', 'piece');
-        blackPawn.setAttribute('data-id', 'piece7' + x);
-        blackPawn.setAttribute('style', 'transform: translate(' + 9 * x + 'vh, 9vh)');
-        pieces.appendChild(blackPawn);
+    // Initialize tiles with appropriate color
+    for (var row = 1; row <= 8; row++) {
+        for (var col = 1; col <= 8; col++) {
+            var tile = document.createElement('div');
+            tile.setAttribute('class', 'tile ' + ((row * 9 + col) & 1 ? 'tile-black' : 'tile-white'));
+            tile.setAttribute('id', 'tile' + (9 - row) + col);
+            tiles.appendChild(tile);
+        }
     }
 
-    for(x = 0; x < 8; x++) {
-        var whitePawn = document.createElement('img');
-        whitePawn.setAttribute('src', 'img/pieces/white/pawn.svg')
-        whitePawn.setAttribute('class', 'piece');
-        whitePawn.setAttribute('data-id', 'piece2' + x);
-        whitePawn.setAttribute('style', 'transform: translate(' + 9 * x + 'vh, 54vh)');
-        pieces.appendChild(whitePawn);
-    }
+    // Initialize pieces at appropriate positions
+    for (var row = 1; row <= 8; row++)
+        for (var col = 1; col <= 8; col++){
+            var tile = document.getElementById('tile' + row + col);
+            var pieceInstance = document.createElement('img');
+            var piece = new InitialPiece(col, row);
+
+            // Check if there should be a piece at all
+            if (piece.type != null) {
+                pieceInstance.src = 'img/pieces/' + piece.color + '/' + piece.type + '.svg';
+                pieceInstance.setAttribute('class', 'piece');
+                tile.appendChild(pieceInstance);
+            }
+        }
 })
 
